b982edd87d71d3e05baea94c11e81efdcee98e5b
Lance Hudson <lancexhudson@gmail.com>
2023-05-24 21:54:48 -0400

    take new photos of merch and update the mobile view

--- src/components/HeroMerch.js
+++ src/components/HeroMerch.js
@@ -7,26 +7,40 @@ import TGblack from '../assets/TGblack.jpeg';
 const HeroMerch = () => {
   return (
     <div className="hero-merch-container">
+      {/* <div className="TEST-container">
+        <div class="TEST-image">
+          <img src={TGwhite} alt="Breathe" className="artwork" />
+        </div>
+        <div class="TEST-info">
+          "Breathe"
+          <p class="description">Sizes: S - XL</p>
+        </div>
+      </div> */}
+
       <div className="merch-container">
         <div id="title">"Breathe"</div>
         {/* <div id="title-sub">subtext</div> */}
-        {/* <div className="artwork"> */}
-        <img src={TGwhite} alt="Breathe" className="artwork" />
-        {/* </div> */}
+        <div className="artwork">
+          <img src={TGwhite} alt="Breathe" className="artwork" />
+          {/* </div> */}
+        </div>
+        <p className="description">Sizes: S - XL</p>
       </div>
       <div className="merch-container">
         <div id="title">"Panther"</div>
         {/* <div id="title-sub">subtext</div> */}
-        {/* <div className="artwork"> */}
-        <img src={TGblue} alt="Panther" className="artwork" />
-        {/* </div> */}
+        <div className="artwork">
+          <img src={TGblue} alt="Panther" className="artwork" />
+        </div>
+        <p className="description">Sizes: S - XL</p>
       </div>
       <div className="merch-container">
         <div id="title">"Dangerous"</div>
         {/* <div id="title-sub">subtext</div> */}
-        {/* <div className="artwork"> */}
-        <img src={TGblack} alt="You Made Me Dangerous" className="artwork" />
-        {/* </div> */}
+        <div className="artwork">
+          <img src={TGblack} alt="You Made Me Dangerous" className="artwork" />
+        </div>
+        <p className="description">Sizes: S - XL</p>
       </div>
     </div>
   );
